37cca9c0d30f54e6116c9ac8ac0585fbe17b5885
nsokil <nsokil@kountable.com>
2019-04-25 17:57:13 +0300

    docker-compose to connect pyweb and mysql

--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__, )
-app.config.from_object('config')
+app.config.from_pyfile('../config/default.py')
 # needs to be passed in arguments, potentially defined in docker-compose '/instance/dev_config.py'
 app.config.from_envvar('APPLICATION_SETTINGS', silent=True)
 
@@ -22,4 +22,3 @@ jwt.init_app(app)
 app.register_blueprint(auth_v1)
 app.register_blueprint(health_v1)
 app.register_blueprint(httperrors)
-# ...
