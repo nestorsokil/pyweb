d5a91d6a394392fa2b3a3329a3831d3df2255cbc
Vitalii <vtomkiv@gmail.com>
2019-04-25 16:28:49 +0300

    DB health check

--- application/__init__.py
+++ application/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from application.shared.database import db
 from application.jwt import jwt
 from application.auth.v1_controller import auth_v1
+from application.http.health_controller import health_v1
 from application.http import httperrors
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-app = Flask(__name__)
+app = Flask(__name__, )
 app.config.from_object('config')
+# needs to be passed in arguments, potentially defined in docker-compose '/instance/dev_config.py'
+app.config.from_envvar('APPLICATION_SETTINGS', silent=True)
 
 db.app = app
 db.init_app(app)
@@ -17,5 +20,6 @@ db.create_all()
 jwt.init_app(app)
 
 app.register_blueprint(auth_v1)
+app.register_blueprint(health_v1)
 app.register_blueprint(httperrors)
 # ...
